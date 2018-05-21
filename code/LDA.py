1b7ff024f0bbc55768ac7d734fdcb1cb26fccecb
thebali <rahulrdb18@gmail.com>
2018-05-21 11:01:27 +0530

    Auto-committing 'LDA.py'

--- code/LDA.py
+++ code/LDA.py
@@ -1,7 +1,7 @@
 import graphlab as gl
 
 
-sarray = gl.SFrame("data/digitalmusic/reviews/complete_reviews_Digital_Music.csv", dtyppe=str)
+sf = gl.SFrame("data/digitalmusic/reviews/complete_reviews_Digital_Music.csv", dtyppe=str)
 
 print sarray.head()
 
