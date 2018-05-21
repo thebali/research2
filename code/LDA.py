320a2723d9cc0947c2cf0145aec265f8a6fad3b5
thebali <rahulrdb18@gmail.com>
2018-05-21 11:01:02 +0530

    Auto-committing 'LDA.py'

--- code/LDA.py
+++ code/LDA.py
@@ -1,7 +1,7 @@
 import graphlab as gl
 
 
-sarray = gl.SArray("data/digitalmusic/reviews/complete_reviews_Digital_Music.csv")
+sarray = gl.SArray("data/digitalmusic/reviews/complete_reviews_Digital_Music.csv", dtyppe=str)
 
 print sarray.head()
 
