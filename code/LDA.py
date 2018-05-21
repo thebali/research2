1e3f22953632e3bd3eaf47136a6a1f592d8327f7
thebali <rahulrdb18@gmail.com>
2018-05-21 11:03:47 +0530

    Auto-committing 'LDA.py'

--- code/LDA.py
+++ code/LDA.py
@@ -1,7 +1,7 @@
 import graphlab as gl
 
 
-sf = gl.SFrame("data/digitalmusic/reviews/complete_reviews_Digital_Music.json")
+sf = gl.SFrame("data/digitalmusic/reviews/complete_reviews_Digital_Music.csv")
 
 print sf.head()
 
