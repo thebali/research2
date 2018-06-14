3a08374d53207b03fb6fd951080fef6adc49b9a1
thebali <rahulrdb18@gmail.com>
2018-06-15 01:01:54 +0530

    new code

--- code/graph-code/data.py
+++ code/graph-code/data.py
@@ -1,3 +1,12 @@
 import json
+import pandas as pd
 
+filelocation = "./dataset/amazon5core/digitalmusic/"
+# filelocation = "./dataset/amazon5core/healthcare/"
 
+file = pd.read_json(filelocation+"Digital_Music_5.json", lines=True)
+# file = pd.read_json(filelocation+"Health_and_Personal_Care_5.json", lines=True)
+
+file = file[["asin", "reviewerID", "overall", "reviewText"]]
+file = file.sample(n=10000, random_state=23) # frac=0.75   for 75% of the row in the data
+file.to_csv(filelocation+"10ksample.csv", sep=",", encoding='utf-8')
