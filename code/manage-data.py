1e9a6ba72c00e61dadc83e400e192306d5fbd0e6
thebali <rahulrdb18@gmail.com>
2018-06-30 17:26:49 +0530

    hgf

--- code/manage-data.py
+++ code/manage-data.py
@@ -1,14 +1,14 @@
 import json
 import pandas as pd
 
-filelocation = "./data/amazon5core/digitalmusic/"
+filelocation = "./data/amazon5core/cellphone/"
 # filelocation = "./data/amazon5core/healthcare/"
 
-file = pd.read_json(filelocation+"Digital_Music_5.json", lines=True)
+file = pd.read_json(filelocation+"Cell_Phones_and_Accessories_5.json", lines=True)
 # file = pd.read_json(filelocation+"Health_and_Personal_Care_5.json", lines=True)
 
 file = file[["asin", "reviewerID", "overall", "reviewText"]]
 file = file.dropna()
 file = file.sample(frac=1, random_state=23) # frac=0.75   for 75% of the row in the data
-file.to_csv(filelocation+"healthcare-notna.csv", sep=",", encoding='utf-8')
+file.to_csv(filelocation+"cellphone5core.csv", sep=",", encoding='utf-8')
 
